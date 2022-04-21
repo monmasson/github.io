0af11b2cc9abba799a071ad12fe18b88cf335df4
Krsna <monicamasson@gmail.com>
2022-04-21 12:49:01 -0400

    commit

--- projectgame.js
+++ projectgame.js
@@ -1,21 +1,30 @@
-const startButton = document.getElementById('start-btn')
-const questionBox = document.getElementById('question-box')
-const questionElement= document.getElementById('question')
-const answersElement = document.getElementById('answers')
-let shuffleQuestions 
-let currentQuestion
+const startButton = document.getElementById("start-btn")
+const questionBox = document.getElementById("question-box")
+const questionElement= document.getElementById("question")
+const answersElement = document.getElementById("answers")
+const restartButton = document.getElementById("restartGame")
+const nextButton = document.getElementById("next")
+let shuffleQuestions ={}
+let currentQuestion={}
+let score=0
+const correctAnswer = 2;
 
 
 
 
 
 startButton.addEventListener('click', startGame)
+restartButton.addEventListener('click',restartGame )
+nextButton.addEventListener('click' , next)
 
-function startGame(){
+
+function startGame()
+{
   // Checking in console console.log('start')// //yay!!! works!!!
- // console.log('start')//
- shuffleQuestions=questions.sort(() => Math.random())
- currentQuestion=0
+ console.log('start')
+ shuffleQuestions=questions.sort(() => Math.floor(Math.random() *  questions.length),
+ currentQuestion=0,
+ score=0
 }
 
 function nextQuestion(){
@@ -30,13 +39,16 @@ function selectAnswer(){}
 //need a loop for questions and answers.//
 
 
-const questions =[
+const questions = [
   {
     question: "Inside which HTML element do we put the JavaScript?",
-    answer: [
-      { text: '<script>' , correct: true},
-      { text: '<js>', correct : false},
+    answers: [
+      {option: '<script>' , correct: true},
+      { option:'<js>', correct : false},
     ]
   },
   //need more questions and answers//
 ]
+
+function restartGame(){}
+function next(){}

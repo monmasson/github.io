88ad9004ff91922950772aa1f7acaeadc3dca030
Krsna <monicamasson@gmail.com>
2022-04-21 09:58:30 -0400

    commit

--- projectgame.js
+++ projectgame.js
@@ -1,78 +1,42 @@
-
-const difficultyLevel = document.getElementById("DIFFICULTY LEVEL");
-const resultsContainer = document.getElementById("results");
-const startButton = document.getElementById("startGame");
-const restartButton = document.getElementById("restartGame");
-const trueBtn = document.getElementById("True");
-const falseBtn = document.getElementById("False");
-const player1Score = document.getElementById("player1-score");
-const player2Score = document.getElementById("player2-score");
-const ul= document.getElementById('ul');
-const nextButton =document.getElementById('buttonNext')
-const questionBox = document.getElementsById("questionBox")
-const opt1 =document.getElementsById('opt1')
-const opt2 =document.getElementsById('opt2')
+const startButton = document.getElementById('startGame')
+const questionbox = document.getElementsById('question-box')
+const question= document.getElementById('questions')
+const answers = document.getElementById('answers')
+let shuffleQuestions = shuffleQuestions
+let currentQuestion= currentQuestion
 
 
 
 
+startButton.addEventListener('click', playGame)
 
-function questionBlock(){}
-function selectAnswers(){}
-function showResults(){}
-function player1Id(){}
-function player2Id(){}
+function playGame(){
+  // Checking in console console.log('start')// //yay!!! works!!!
+ // console.log('start')//
+ shuffleQuestions=questions.sort(() => Math.random())
+ currentQuestion=0
 
-
-var correct = 0;
-var incorrect = 0;
-var question = "none";
-var answer = "none";
-var answer = "none";
-
-let currentQuestion = 0;
-
-const javaQuestions = [
-    {
-        question1: "need question" ,
-        answer1:{
-            1: "need inputs",
-            2 : "need inputs",
-} ,
-correctAnswer: "need input",
-    } ,
-
-   {
-        question2: "need question" ,
-        answer2:{
-            1: "need inputs",
-            2 : "need inputs",
-} ,
-correctAnswer: "need input",
-    } ,
-
-    
-]
-
-restartButton.addEventListener("click", restart);
-startButton.addEventListener("click", startGame);
-
-function calculateCorrectAnswers() {
-    let count = 0;
-    this.questions.forEach(el => {
-      if (el.isCorrect) {
-        count++;
-      }
-    });
-    return count;
-  }
-
-  var score = function(){ 
-	if(input == answer){ 
-		correct = correct+1;
-	
-	}else{
-		incorrect = incorrect+1;
-
-	}
+ 
+ 
+  
 }
+
+function showQuestion(questions){}
+
+
+function nextQuestion(){
+  showQuestion(shuffleQuestions[currentQuestion])
+}
+
+//function selectAnswer(){}
+
+
+const questions =[
+  {
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: [
+      { text: '<script>' , correct: true},
+            { text: '<js>', correct : false},
+    ]
+  }
+]

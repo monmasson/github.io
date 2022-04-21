3c4019f1333b59bada9e4912a7295773d6639a1e
Krsna <monicamasson@gmail.com>
2022-04-21 12:01:54 -0400

    commit

--- projectgame.js
+++ projectgame.js
@@ -1,30 +1,33 @@
-const startButton = document.getElementById('startGame')
-const questionboxElement = document.getElementsById('question-box')
-const questionElement= document.getElementById('questions')
+const startButton = document.getElementById('start-btn')
+const questionBox = document.getElementById('question-box')
+const questionElement= document.getElementById('question')
 const answersElement = document.getElementById('answers')
-let shuffleQuestions = shuffleQuestions
-let currentQuestion= currentQuestion
+let shuffleQuestions 
+let currentQuestion
 
 
 
 
-startButton.addEventListener('click', playGame)
 
-function playGame(){
+startButton.addEventListener('click', startGame)
+
+function startGame(){
   // Checking in console console.log('start')// //yay!!! works!!!
  // console.log('start')//
  shuffleQuestions=questions.sort(() => Math.random())
  currentQuestion=0
 }
 
-function showQuestion(questions){}
-//need a loop for questions and answers.//
-
-
 function nextQuestion(){
-  showQuestion(shuffleQuestions[currentQuestion])
+  showQuestion(shuffleQuestions[currentQuestion])// for random question//
 }
-//function selectAnswer(){}
+
+function showQuestion(question){
+  questionElement.innerText = question.question
+}
+
+function selectAnswer(){}
+//need a loop for questions and answers.//
 
 
 const questions =[
@@ -32,9 +35,8 @@ const questions =[
     question: "Inside which HTML element do we put the JavaScript?",
     answer: [
       { text: '<script>' , correct: true},
-            { text: '<js>', correct : false},
+      { text: '<js>', correct : false},
     ]
-  }
+  },
   //need more questions and answers//
 ]
-function test1(){}
